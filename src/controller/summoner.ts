cd2ac5bb5d7549aee4d5e839feecd1acb0dd9dd0
Nicolas Vera <nicolasveraa97@gmail.com>
2022-08-26 17:54:54 -0300

    Add filter to avoid tft ranked data

--- src/controller/summoner.ts
+++ src/controller/summoner.ts
@@ -8,7 +8,11 @@ const getRankData = async (id:string) =>{
     const {data:rankedData} = await axios.get(`${URL}/lol/league/v4/entries/by-summoner/${id}?api_key=${process.env.RIOT_API}`)
 
     if(rankedData.length){
-        return rankedData
+        const tftRegex = new RegExp(/TFT/)
+        const riftData = rankedData.filter((queue:any)=>{
+            return tftRegex.test(queue.queueType) === false
+        })
+        return riftData
     } else {
         return 'unranked'
     }
